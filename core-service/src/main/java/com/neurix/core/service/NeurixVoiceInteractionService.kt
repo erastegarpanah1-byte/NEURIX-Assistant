684c4457f386987e10f354b55c99e293c2d9ff12
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-08-01 17:05:31 +0330

    Fix onStartSession override signature

--- core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionService.kt
+++ core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionService.kt
@@ -1,6 +1,5 @@
 package com.neurix.core.service
 
-import android.os.Bundle
 import android.service.voice.VoiceInteractionService
 import android.service.voice.VoiceInteractionSession
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,5 +7,5 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class NeurixVoiceInteractionService : VoiceInteractionService() {
     override fun onReady() { super.onReady() }
-    override fun onStartSession(args: Bundle?): VoiceInteractionSession = NeurixVoiceInteractionSession(this)
+    override fun onStartSession(args: android.os.Bundle?): VoiceInteractionSession = NeurixVoiceInteractionSession(this)
 }
