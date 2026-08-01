1462c84755eedc9ac8759bc444ca4f876a9f0291
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-08-01 16:41:18 +0330

    Add Hilt dependency to core-common module

--- core-common/build.gradle.kts
+++ core-common/build.gradle.kts
@@ -1,21 +1,32 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.neurix.core.common"
     compileSdk = 35
-    defaultConfig { minSdk = 24 }
+
+    defaultConfig {
+        minSdk = 24
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
     implementation(libs.core.ktx)
     implementation(libs.coroutines.core)
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.2")
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
