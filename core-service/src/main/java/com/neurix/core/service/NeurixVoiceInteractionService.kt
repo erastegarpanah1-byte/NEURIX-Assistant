e5d10181f9d5fa3695d9b07c631fc37150e0c8fd
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-08-01 17:08:57 +0330

    Fix VoiceInteractionService: remove onStartSession, add secondary constructor to session, fix xml

--- core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionService.kt
+++ core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionService.kt
@@ -1,11 +1,9 @@
 package com.neurix.core.service
 
 import android.service.voice.VoiceInteractionService
-import android.service.voice.VoiceInteractionSession
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NeurixVoiceInteractionService : VoiceInteractionService() {
     override fun onReady() { super.onReady() }
-    override fun onStartSession(args: android.os.Bundle?): VoiceInteractionSession = NeurixVoiceInteractionSession(this)
 }
--- core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionSession.kt
+++ core-service/src/main/java/com/neurix/core/service/NeurixVoiceInteractionSession.kt
@@ -1,12 +1,16 @@
 package com.neurix.core.service
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.service.voice.VoiceInteractionSession
 
-class NeurixVoiceInteractionSession(s: NeurixVoiceInteractionService) : VoiceInteractionSession(s) {
+class NeurixVoiceInteractionSession : VoiceInteractionSession {
+    constructor(s: NeurixVoiceInteractionService) : super(s)
+    constructor(c: Context) : super(c)
+
     override fun onCreate() { super.onCreate(); open() }
-    override fun onHandleAssist(d: Bundle?, as1: android.app.assist.AssistStructure?, ac: android.app.assist.AssistContent?) { open() }
+    override fun onHandleAssist(d: Bundle?, s: android.app.assist.AssistStructure?, c: android.app.assist.AssistContent?) { open() }
     private fun open() {
         context.startActivity(Intent().apply {
             setClassName(context, "com.neurix.app.MainActivity")
